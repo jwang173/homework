016f35cd4036008724d2ccc854f282c6df2365cb
jwang173 <jwang173@stevens.edu>
2020-09-23 08:45:33 +0800

    delete create

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -3,24 +3,42 @@ import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-list',
-  template:`
-    <h2>Post List</h2>
-    <ul *ngFor="let post of posts">
-      <li>{{post.id}}:{{post.title}}</li>
-    </ul>
-  `,
+  // template:`
+  //   <h2>Post List</h2>
+  //   <ul *ngFor="let post of posts">
+  //     <li>{{post.id}} {{post.title}} {{post.body}}</li>
+  //   </ul>
+  // `,
+  templateUrl:'./post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
 
-  public posts = [];
+  public title = 'Homework';
+  public posts:any = [];
 
   constructor(private _postService: PostService) { }
 
   ngOnInit(): void {
     this._postService.getPosts()
-        .subscribe(data => this.posts =data);
+        .subscribe(data => {
+          console.log(data);
+          this.posts =data;
+        });
   }
 
+  deletePost(id) {
+    this.posts = this.posts.filter(item => {
+      return item.id !== id;
+    })
+    this._postService.deletePost(id).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
 
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -12,11 +12,16 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts():Observable<IPost[]> {
-    return this.http.get<IPost[]>(this._url);
+  getPosts() {
+    return this.http.get(this._url);
   }
 
-  // createPosts(){
-
+  deletePost(id){
+    return this.http.delete(this._url+"/id");
+  }
+  
+  // createPosts(id:number,title:string){
+  //   const postData: IPost={id:id,title:title};
+  //   this.http.post(this._url,postData).subscribe();
   // }
 }
